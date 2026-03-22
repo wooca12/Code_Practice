2fd00c09b27ac9966b9a4c964a7a00298d317ef0
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-03-22 12:37:13 +0900

    Title: 흰검 칠하기, Time: 74ms, Memory: 27MB, Status: Wrong Answer - Codetree

--- 260322/흰검 칠하기/painting-white-black.py
+++ 260322/흰검 칠하기/painting-white-black.py
@@ -8,15 +8,16 @@ count_w = [0] * (2 * OFFSET + 1)
 cur_index = OFFSET
 for num, direction in commands:
     if direction == 'L':
-        for i in range(cur_index, cur_index + int(num), 1):
+        for i in range(cur_index, cur_index - int(num), -1):
             blocks[i] = 1
             count_w[i] += 1
             cur_index = i
     elif direction == 'R':
-        for i in range(cur_index, cur_index - int(num), -1):
+        for i in range(cur_index, cur_index + int(num), 1):
             blocks[i] = 2
             count_b[i] += 1
             cur_index = i
+    print(blocks)
 
 w, b, g = 0, 0, 0
 for i in range(len(blocks)):
@@ -27,4 +28,6 @@ for i in range(len(blocks)):
     elif blocks[i] == 2:
         b += 1
 print(w, b, g)
+
+
 
