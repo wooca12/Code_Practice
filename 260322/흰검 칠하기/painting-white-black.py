ddf3d06dff1e3d84186884071fe6d8ff24b4a1e0
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-03-22 12:33:07 +0900

    Title: 흰검 칠하기, Time: 84ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 260322/흰검 칠하기/painting-white-black.py
+++ 260322/흰검 칠하기/painting-white-black.py
@@ -1,65 +1,30 @@
 n = int(input())
-x = []
-dir = []
-for _ in range(n):
-    xi, di = input().split()
-    x.append(int(xi))
-    dir.append(di)
+commands = [tuple(input().split()) for _ in range(n)]
 
-# L : 왼쪽, 현재 포함 x칸 흰색
-# R : 오른쪽, 현재 포함 x 검은칸
+OFFSET = 1000
+blocks = [0] * (2 * OFFSET + 1)
+count_b = [0] * (2 * OFFSET + 1)
+count_w = [0] * (2 * OFFSET + 1)
+cur_index = OFFSET
+for num, direction in commands:
+    if direction == 'L':
+        for i in range(cur_index, cur_index + int(num), 1):
+            blocks[i] = 1
+            count_w[i] += 1
+            cur_index = i
+    elif direction == 'R':
+        for i in range(cur_index, cur_index - int(num), -1):
+            blocks[i] = 2
+            count_b[i] += 1
+            cur_index = i
 
-# 흰 검 회
-# 1
-offset = 10000
-blocks = [0] * 20000
-cur = 0
-cur += offset
-for i in range(n):
-    if dir[i] == 'L':
-        for j in range(cur, cur - x[i], -1):
-            blocks[j] = blocks[j] * 10 + 1
-        cur = j
-    elif dir[i] == 'R':
-        for j in range(cur, cur + x[i], 1):
-            blocks[j] = blocks[j] * 10 + 2
-        cur = j
-
-
-def is_gray(num):
-    black_count = 0
-    white_count = 0
-    while True:
-        if num < 10:
-            if num % 10 == 1:
-                white_count += 1
-            elif num % 10 == 2:
-                black_count += 1
-            break
-        if num % 10 == 1:
-            white_count += 1
-        elif num % 10 == 2:
-            black_count += 1
-        num //= 10
-
-    if white_count >= 2 and black_count >= 2:
-        return True
-
-    return False
-
-count = [0, 0, 0]
-
+w, b, g = 0, 0, 0
 for i in range(len(blocks)):
-    if blocks[i] >= 1000 and is_gray(blocks[i]):
-        count[2] += 1 # 회색
-    else:
-        blocks[i] = blocks[i] % 10
-        if blocks[i] == 2:
-            count[1] += 1 # 검은색
-        elif blocks[i] == 1:
-            count[0] += 1
-
-for i in count:
-    print(i, end=' ')
-
+    if count_b[i] == 2 and count_w[i] == 2:
+        g += 1
+    elif blocks[i] == 1:
+        w += 1
+    elif blocks[i] == 2:
+        b += 1
+print(w, b, g)
 
