d12aa3a86204ae62747bfe5bdc6df232ce6ad379
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-03-22 12:48:30 +0900

    Title: 흰검 칠하기, Time: 1083ms, Memory: 16MB, Status: Time Limit Exceed - Codetree

--- 260322/흰검 칠하기/painting-white-black.py
+++ 260322/흰검 칠하기/painting-white-black.py
@@ -1,44 +1,35 @@
-MAX_K = 100000
-# 변수 선언 및 입력:
 n = int(input())
-a = [0] * (2 * MAX_K + 1)
-cnt_b = [0] * (2 * MAX_K + 1)
-cnt_w = [0] * (2 * MAX_K + 1)
-b, w, g = 0, 0, 0
+commands = [tuple(input().split()) for _ in range(n)]
 
-cur = MAX_K
-for _ in range(n):
-    x, c = tuple(input().split())
-    x = int(x)
+OFFSET = 10
+blocks = [0] * (2 * OFFSET + 1)
+count_b = [0] * (2 * OFFSET + 1)
+count_w = [0] * (2 * OFFSET + 1)
+cur_index = OFFSET
+for num, direction in commands:
+    num = int(num)
+    if direction == 'L':
+        while num > 0:
+            for i in range(cur_index, cur_index - num, 1):
+                blocks[i] = 1
+                count_w[i] += 1
+                cur_index = i
+    elif direction == 'R':
+        for i in range(cur_index, cur_index + num, 1):
+            blocks[i] = 2
+            count_b[i] += 1
+            cur_index = i
+    print(blocks)
 
-    if c == 'L':
-        # x칸 왼쪽으로 칠합니다.
-        while x > 0:
-            a[cur] = 1
-            cnt_w[cur] += 1
-            x -= 1
-
-            if x: 
-                cur -= 1
-    else:
-        # x칸 오른쪽으로 칠합니다.
-        while x > 0:
-            a[cur] = 2
-            cnt_b[cur] += 1
-            x -= 1
-
-            if x: 
-                cur += 1
-
-for i in range(2 * MAX_K + 1):
-    # 검은색과 흰색으로 두 번 이상 칠해진 타일은 회색입니다.
-    if cnt_b[i] >= 2 and cnt_w[i] >= 2: 
+w, b, g = 0, 0, 0
+for i in range(len(blocks)):
+    if count_b[i] >= 2 and count_w[i] >= 2:
         g += 1
-    # 그렇지 않으면 현재 칠해진 색깔이 곧 타일의 색깔입니다.
-    elif a[i] == 1: 
+    elif blocks[i] == 1:
         w += 1
-    elif a[i] == 2: 
+    elif blocks[i] == 2:
         b += 1
-
-# 정답을 출력합니다.
 print(w, b, g)
+
+
+
