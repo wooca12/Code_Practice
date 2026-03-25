2071062e10f29a0288f275ff707ce8b51e1320fb
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-03-25 14:50:43 +0900

    Title: 만나는 그 순간, Time: 115ms, Memory: 20MB, Status: Runtime Error - Codetree

--- 260325/만나는 그 순간/the-moment-we-meet.py
+++ 260325/만나는 그 순간/the-moment-we-meet.py
@@ -1,16 +1,25 @@
-OFFSET = 10000
+# A: 9s front / 3s back  / 5s front
+# B; 2s back / 2s front / 12s front
+# time: A=B and position A=B
+OFFSET = 20
 MAX = 2 * OFFSET
 
 n, m = map(int, input().split())
 
 a_commend = []
 b_commend = []
+a_times = 0
+b_times = 0
 for _ in range(n):
-    a_commend.append(tuple(input().split()))
+    d, t = input().split()
+    a_commend.append((d, t))
+    a_times += int(t)
 for _ in range(m):
-    b_commend.append(tuple(input().split()))
+    d, t = input().split()
+    b_commend.append((d, t))
+    b_times += int(t)
 
-def simulator(n, commands):
+def simulator(commands):
     array = [0] * (MAX + 1)
     cur_p = 0
     cur_s = 0
@@ -28,11 +37,17 @@ def simulator(n, commands):
     return array
 
 
-a_array = simulator(n, a_commend)
-b_array = simulator(m, b_commend)
+a_array = simulator(a_commend)
+b_array = simulator(b_commend)
 
 
+ans = -1
 for i in range(MAX + 1):
-    if i !=0 and a_array[i] == b_array[i]:
-        print(i)
+    if i != 0 and a_array[i] == b_array[i]:
+        ans = i
         break
+    if a_times == 0 or b_times == 0:
+        break
+    a_times -=1
+    b_times -=1
+print(ans)
