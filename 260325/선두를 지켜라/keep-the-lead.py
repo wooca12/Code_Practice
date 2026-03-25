ec0550df3d515903dea50d000ff1048c43485005
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-03-25 16:32:19 +0900

    Title: 선두를 지켜라, Time: 105ms, Memory: 30MB, Status: Passed - Codetree

--- 260325/선두를 지켜라/keep-the-lead.py
+++ 260325/선두를 지켜라/keep-the-lead.py
@@ -1,4 +1,3 @@
-from binascii import a2b_base64
 
 n, m = map(int, input().split())
 
@@ -26,12 +25,13 @@ for v, times in b_moves:
 first, count = 0, 0
 
 for i in range(len(a_dis)):
-    if first == 0:
+    if first == 0 and a_dis[i] != b_dis[i]:
         first = 1 if a_dis[i] < b_dis[i] else 2
-    elif first == 1 and a_dis[i] < b_dis[i]:
+    elif first != 2 and a_dis[i] > b_dis[i]:
         first = 2
         count += 1
-    elif first == 2 and a_dis[i] > b_dis[i]:
+    elif first != 1 and a_dis[i] < b_dis[i]:
         first = 1
         count += 1
+
 print(count)
