c9d78facf15f10f71e4d535f6bfa6b5233a7257a
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-03-26 22:30:59 +0900

    Title: 일렬로 서있는 소 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260326/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -0,0 +1,9 @@
+N = int(input())
+A = list(map(int, input().split()))
+count = 0
+for i in range(N):
+    for j in range(i + 1, N):
+        for k in range(j + 1, N):
+            if A[i] <= A[j] and A[j] <= A[k]:
+                count += 1
+print(count)
