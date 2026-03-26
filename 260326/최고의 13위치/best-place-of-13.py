70cc3b6dc3624b01b32c4f82698bff23219a987c
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-03-26 22:35:37 +0900

    Title: 최고의 13위치, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260326/최고의 13위치/best-place-of-13.py
+++ 260326/최고의 13위치/best-place-of-13.py
@@ -2,6 +2,6 @@ n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 max_sum = 0
 for i in range(n):
-    for j in range(i, n-2):
+    for j in range(n-2):
         max_sum = max(max_sum, grid[i][j] + grid[i][j+1] + grid[i][j+2])
 print(max_sum)
