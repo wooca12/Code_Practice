7a7ffa2d580cd60c5a8b96891b7463fb0cf91c49
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-03-26 10:49:00 +0900

    Title: 좌우로 움직이는 로봇, Time: 286ms, Memory: 63MB, Status: Passed - Codetree

---
+++ 260326/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -0,0 +1,45 @@
+n, m = map(int, input().split())
+
+# Process robot A's movements
+t = []
+d = []
+for _ in range(n):
+    time, direction = input().split()
+    t.append(int(time))
+    d.append(direction)
+
+# Process robot B's movements
+t_b = []
+d_b = []
+for _ in range(m):
+    time, direction = input().split()
+    t_b.append(int(time))
+    d_b.append(direction)
+
+def time_position(t, d):
+    array = [0]
+    for s, l in zip(t, d):
+        for i in range( s):
+            if l == 'R':
+                array.append(array[-1] + 1)
+            else:
+                array.append(array[-1] - 1)
+    return array
+
+a_position = time_position(t, d)
+b_position = time_position(t_b, d_b)
+
+count = 0
+
+a_id, b_id = 0, 0
+for i in range(1, max(len(a_position), len(b_position))):
+    a_id = i if i < len(a_position) else a_id
+    b_id = i if i < len(b_position) else b_id
+    pre_a = i - 1 if i < len(a_position) else a_id
+    pre_b = i - 1 if i < len(b_position) else b_id
+    if a_position[a_id] == b_position[b_id] and a_position[pre_a] != b_position[pre_b]:
+        count += 1
+
+
+
+print(count)
