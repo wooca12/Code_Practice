88b4a3fc65e33766aeec3e52b41bb1e0f3093474
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-03-27 21:52:39 +0900

    Title: 특정 수와 근접한 합, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260327/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 260327/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -6,10 +6,11 @@ min_diff = sys.maxsize
 total_sum = sum(arr)
 
 for i in range(n-1):
-    for j in range(i, n):
+    for j in range(i + 1, n):
         sum = total_sum - (arr[i] + arr[j])
         min_diff = min(min_diff, abs(sum - s))
 print(min_diff)
+
 
 
 
