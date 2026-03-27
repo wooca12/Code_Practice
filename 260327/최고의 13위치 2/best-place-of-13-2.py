6788fa17738f0f000441cdf218fe687cb1aa1975
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-03-27 17:39:14 +0900

    Title: 최고의 13위치 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260327/최고의 13위치 2/best-place-of-13-2.py
+++ 260327/최고의 13위치 2/best-place-of-13-2.py
@@ -1,22 +1,23 @@
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
-#    좌  하  우  상
-dxs = [0, 1, 0, -1]
-dys = [1, 0, -1, 0]
+#    우  하  좌  상
+dxs = [0, 1]
+dys = [1, 0]
+
 max_count = 0
 def in_range(x, y):
     return 0 <= x < n and 0 <= y < n
 
-
 for i in range(n):
     for j in range(2, n):
-        max_count += arr[i][j-2] + arr[i][j-1] + arr[i][j]
+        count1 = arr[i][j-2] + arr[i][j-1] + arr[i][j]
         for dx, dy in zip(dxs, dys):
             curx, cury = i, j
             while True:
                 nx, ny = curx + dx, cury + dy
                 if not in_range(nx, ny):
                     break
-                max_cnt = max(max_count, max_count + arr[nx][ny-2] + arr[nx][ny-1] + arr[nx][ny])
-                curx, cury = curx + dx, cury + dy
+                count2 = arr[nx][ny-2] + arr[nx][ny-1] + arr[nx][ny]
+                max_count = max(max_count, count1 + count2)
+                curx, cury = nx, ny
 print(max_count)
