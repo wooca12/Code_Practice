56324e1ec64cc15285be4434a6b56fb1675e1d02
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-03-27 13:20:38 +0900

    Title: 원 모양으로 되어있는 방, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260327/원 모양으로 되어있는 방/a-room-in-a-circle.py
@@ -0,0 +1,14 @@
+import sys
+n = int(input())
+a = [int(input()) for _ in range(n)]
+
+min_dist = sys.maxsize
+for i in range(n): # i 시작 방
+    dist = 0
+    for j in range(n):
+        d = j - i
+        d = d if d >= 0 else d + 5 # 사람마다 가야할 거리
+        dist += a[j] * d
+    min_dist = min(min_dist, dist)
+
+print(min_dist)
