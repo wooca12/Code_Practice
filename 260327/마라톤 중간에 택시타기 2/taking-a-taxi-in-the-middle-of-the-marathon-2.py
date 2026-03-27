ea6b1fc57096599d3a2411bd5c73134b117a48e9
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-03-27 12:54:56 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260327/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ 260327/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -6,12 +6,13 @@ y = [p[1] for p in points]
 
 min_dist = sys.maxsize
 dist = 0
-for i in range(1, n - 1):
-    # 점프 구간 i 번째
-    new_dist = dist + abs(x[i + 1] - x[i-1]) + abs(y[i + 1] - y[i-1])
-    for j in range(i + 1, n - 1):
-        new_dist = new_dist + abs(x[j + 1] - x[j]) + abs(y[j + 1] - y[j])
+for i in range(1, n - 1): # 처음 끝 제외한 점프 가능한 구간
+    # 점프 구간 i 번째 제외한 거리
+    new_dist = dist + abs(x[i-1] - x[i+1]) + abs(y[i-1] - y[i+1])
+    # 점프 구간 이후 거리
+    for j in range(i + 2, n):
+        new_dist += abs(x[j-1] -x[j]) + abs(y[j-1] -y[j]) 
     min_dist = min(min_dist, new_dist)
-    dist += abs(x[i] - x[i-1]) + abs(y[j] - y[i - 1])
+    dist += abs(x[i] - x[i-1]) + abs(y[i] - y[i - 1]) 
 
 print(min_dist)
