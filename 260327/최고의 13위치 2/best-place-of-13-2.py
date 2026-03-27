a6717f160a6cf50b43cb268777d48680770c518e
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-03-27 21:14:03 +0900

    Title: 최고의 13위치 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260327/최고의 13위치 2/best-place-of-13-2.py
+++ 260327/최고의 13위치 2/best-place-of-13-2.py
@@ -1,26 +1,27 @@
+# 변수 선언 및 입력
 n = int(input())
-arr = [list(map(int, input().split())) for _ in range(n)]
-#    우  하  좌  상
-dxs = [0, 1]
-dys = [1, 0]
+arr = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
 
-max_count = 0
-def in_range(x, y):
-    return 0 <= x < n and 0 <= y < n
-
+# Step 1.
+# 첫 번째 격자를 놓습니다. (i , j)
+max_cnt = 0
 for i in range(n):
-    for j in range(2, n):
-        count1 = arr[i][j-2] + arr[i][j-1] + arr[i][j]
+    # 격자를 벗어나지 않을 범위로만 잡습니다.
+    for j in range(n - 2):
+        # 두 번째 격자를 놓습니다. (k , l)
+        for k in range(n):
+            # 격자를 벗어나지 않을 범위로만 잡습니다.
+            for l in range(n - 2):
+                # Step2. 두 격자가 겹치는 경우에는 가짓수로 세지 않습니다.
+                if i == k and abs(j - l) <= 2:
+                    continue
 
-        for dx, dy in zip(dxs, dys):
-            curx, cury = i, j
-            while True:
-                nx, ny = curx + dx, cury + dy
-                if not in_range(nx, ny):
-                    break
-                if i == nx and abs(j-ny) < 3 :
-                    break
-                count2 = arr[nx][ny-2] + arr[nx][ny-1] + arr[nx][ny]
-                max_count = max(max_count, count1 + count2)
-                curx, cury = nx, ny
-print(max_count)
+                # Step 3. 두 격자가 겹치지 않는 경우에 대해 동전 수를 세어 갱신해줍니다.
+                cnt1 = arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
+                cnt2 = arr[k][l] + arr[k][l + 1] + arr[k][l + 2]
+                max_cnt = max(max_cnt, cnt1 + cnt2)
+
+print(max_cnt)
