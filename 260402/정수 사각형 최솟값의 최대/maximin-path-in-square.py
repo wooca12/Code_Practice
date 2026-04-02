01a27bd7504c58a348d9f55cf62d67698bca6b86
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-02 20:18:02 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260402/정수 사각형 최솟값의 최대/maximin-path-in-square.py
@@ -0,0 +1,21 @@
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+dp = [[0] * n for _ in range(n)]
+
+def initalize():
+    dp[0][0] = grid[0][0]
+
+    for i in range(1, n):
+        dp[i][0] = min(dp[i-1][0], grid[i][0])
+
+    for i in range(1, n):
+        dp[0][i] = min(dp[0][i-1], grid[0][i])
+
+initalize()
+
+for i in range(1, n):
+    for j in range(1, n):
+        dp[i][j] = min(max((dp[i-1][j], dp[i][j-1])), grid[i][j])
+
+print(dp[n-1][n-1])
