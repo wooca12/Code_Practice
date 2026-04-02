ea6956aacbe7aeefd7325a752b2dbb6a5b2f48d6
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-02 17:14:01 +0900

    Title: 계단 오르기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260402/계단 오르기/climbing-stairs.py
@@ -0,0 +1,15 @@
+N = int(input())
+
+memo = [-1] * 1001
+
+def solution(n):
+    if memo[n] != -1:
+        return memo[n]
+    if n <= 3:
+        memo[n] = 1
+    else:
+        memo[n] = solution(n-2) + solution(n-3)
+    return memo[n]
+
+print(solution(N))
+        
