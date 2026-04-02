82f013e0c8f5dde5683f484e22766a96206a7c6b
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-02 18:25:07 +0900

    Title: 사각형 채우기 3, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260402/사각형 채우기 3/rectangle-fill-3.py
+++ 260402/사각형 채우기 3/rectangle-fill-3.py
@@ -1,3 +1,4 @@
+
 n = int(input())
 
 memo = [-1] * 1001
@@ -5,13 +6,17 @@ memo = [-1] * 1001
 def solve(n):
     if memo[n] != -1:
         return memo[n]
-    if n <= -1:
+    if n < 0:
         memo[n] = 0
     elif n == 0:
         memo[n] = 1
     elif n == 1:
         memo[n] = 2
     else:
-        memo[n] = 2 * solve(n-1) + 3 * solve(n-2) + 2 * solve(n-3)
+        memo[n] = 2 * solve(n-1) + 3 * solve(n-2)
+        for i in range(n-3, -1, -1):
+            memo[n] = memo[n] + 2 * solve(i)
+
     return memo[n]
-print(solve(n) %1000000007)
+
+print(solve(n) % 1000000007)
