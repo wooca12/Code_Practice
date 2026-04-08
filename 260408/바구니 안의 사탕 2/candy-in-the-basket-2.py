09685ed988a3773fdd3b89a2830a98c3851974ec
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-08 16:19:22 +0900

    Title: 바구니 안의 사탕 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260408/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -0,0 +1,23 @@
+N, K = map(int, input().split())
+candy = []
+pos = []
+
+for _ in range(N):
+    c, p = map(int, input().split())
+    candy.append(c)
+    pos.append(p)
+
+arr = [0] * 101
+
+for i in range(N) :
+    arr[pos[i]] += candy[i]
+
+
+max_num = 0
+for i in range(1, 101 - 2*K):
+    sum = 0
+    for j in range(i, i + 2*K + 1):
+        sum += arr[j]
+    max_num = max(max_num, sum)
+
+print(max_num)
