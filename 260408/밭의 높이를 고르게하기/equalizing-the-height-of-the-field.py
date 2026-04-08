b8e3bc83ad145e16e79a6aa808b65ec9297356c4
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-08 17:25:21 +0900

    Title: 밭의 높이를 고르게하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260408/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
+++ 260408/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -5,10 +5,8 @@ arr = list(map(int, input().split()))
 min_cost = sys.maxsize
 
 for i in range(N - T + 1):
-    for j in range(i + T, N + 1):
-        cost = 0
-        for k in range(i, j):
-            if arr[k] != H:
-                cost += abs(H - arr[k])
-        min_cost = min(min_cost, cost)
+    cost = 0
+    for j in range(i, i + T):
+        cost += abs(H - arr[j])
+    min_cost = min(min_cost, cost)
 print(min_cost)
