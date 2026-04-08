657ed0a87bdbb2f1288b733623e5f296d5202a89
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-08 16:09:00 +0900

    Title: 아름다운 수열 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260408/아름다운 수열 2/beautiful-sequence-2.py
+++ 260408/아름다운 수열 2/beautiful-sequence-2.py
@@ -3,13 +3,9 @@ A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
 count = 0
+
 for i in range(N - M + 1):
     exist = True
-    for j in range(M):
-        if B[j] not in A[i : i + M]:
-            exist = False
-            break
-    if exist:
+    if sorted(B) == sorted(A[i:i+M]):
         count += 1
-
 print(count)
