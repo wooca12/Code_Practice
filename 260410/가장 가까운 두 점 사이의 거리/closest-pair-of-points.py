f48d759cdbf64f53cb82d8edd7144c0e7472594b
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-10 15:25:21 +0900

    Title: 가장 가까운 두 점 사이의 거리, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260410/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
+++ 260410/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
@@ -1,8 +1,9 @@
+import sys
 n = int(input())
 points = [tuple(map(int, input().split())) for _ in range(n)]
 x = [p[0] for p in points]
 y = [p[1] for p in points]
-min_dist = 1000
+min_dist = sys.maxsize
 
 for i in range(n):
     for j in range(i+1, n):
