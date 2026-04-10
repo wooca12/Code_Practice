f82d38b17093b667217ccd6716272758ac83c5bd
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-10 14:25:41 +0900

    Title: 개발자의 능력 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260410/개발자의 능력 2/ability-of-developer-2.py
+++ 260410/개발자의 능력 2/ability-of-developer-2.py
@@ -6,17 +6,20 @@ min_diff = sys.maxsize
 
 def get_diff(i, j, k, m):
     sum1 = ability[i] + ability[j]
-    sum2 = ability[k] + ability[j]
+    sum2 = ability[k] + ability[m]
     sum3 = sum(ability) - sum1 - sum2
     max_sum = max(sum1, sum2, sum3)
     min_sum = min(sum1, sum2, sum3)
-    return max_sum - min_sum
+    return abs(max_sum - min_sum)
 
 for i in range(n):
-    for j in range(i+1, n):
-        for k in range(j+1, n):
-            for m in range(k+1, n):
-                min_diff = min(min_diff, get_diff(i, j, k, m))
-
+    for j in range(n):
+        for k in range(n):
+            for m in range(n):
+                if i != j and i != k and i != m and j != k and j != m and k != m:
+                    min_diff = min(min_diff, get_diff(i, j, k, m))
+                    
+        
+            
 print(min_diff)
                 
