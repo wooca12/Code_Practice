764ed0cee6bcd262e929df39edcbc9d359aa4c68
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-10 21:41:15 +0900

    Title: 개발자의 순위, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

--- 260410/개발자의 순위/developers-rank.py
+++ 260410/개발자의 순위/developers-rank.py
@@ -1,31 +1,53 @@
 k, n = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(k)]
-new_arr = []
 
-for i in range(k):
-    r = [0] * n
-    for j in range(n):
-        rank = j + 1
-        dev_num = arr[i][j] - 1
-        r[dev_num] = rank
 
-    new_arr.append(r)
-    
-
-    
+## Solution #1
 count = 0
-for i in range(n):
-    for j in range(n):
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
         if i == j:
             continue
-        flag = True
+        correct = True
 
-        for l in range(k):
-            contest = new_arr[l]
-            if contest[i] >= contest[j]:
-                flag = False
-                break
-            
-        if flag :
+        for list in arr:
+            id1 = lists.index(i)
+            id2 = lists.index(j)
+            if id1 >= id2:
+                correct = Fasle
+        if correct:
             count += 1
 print(count)
+
+
+## Solution #2
+# new_arr = []
+
+# for i in range(k):
+#     r = [0] * n
+#     for j in range(n):
+#         rank = j + 1
+#         dev_num = arr[i][j] - 1
+#         r[dev_num] = rank
+
+#     new_arr.append(r)
+    
+
+    
+# count = 0
+# for i in range(n):
+#     for j in range(n):
+#         if i == j:
+#             continue
+#         flag = True
+
+#         for l in range(k):
+#             contest = new_arr[l]
+#             if contest[i] >= contest[j]:
+#                 flag = False
+#                 break
+            
+#         if flag :
+#             count += 1
+# print(count)
