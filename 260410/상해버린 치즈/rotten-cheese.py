5bcf681caf4556055ea96faec1c55d4ec0bd50d0
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-10 19:15:21 +0900

    Title: 상해버린 치즈, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260410/상해버린 치즈/rotten-cheese.py
+++ 260410/상해버린 치즈/rotten-cheese.py
@@ -10,11 +10,15 @@
 N, M, D, S = map(int, input().split())
 
 p, m, t = [], [], []
+eat_cheese = [set() for _ in range(N + 1)]
 for _ in range(D):
     person, milk, time = map(int, input().split())
     p.append(person)
     m.append(milk)
     t.append(time)
+
+    eat_cheese[person].add(milk)
+
 
 sick_p, sick_t = [], []
 for _ in range(S):
@@ -22,27 +26,49 @@ for _ in range(S):
     sick_p.append(person)
     sick_t.append(time)
 
-def fail_check_sick(person, time):
-    for s in range(S):
-        if person == sick_p[s] and time >= sick_t[s]:
+def late_sick_time(person, time):
+    for i in range(S):
+        if person == sick_p[i] and time >= sick_t[i]:
             return True
     return False
 
+def sick_but_not_eat_bad(person, bad_cheese):
+    if person in sick_p and bad_cheese not in eat_cheese[person]:
+        return True
+    return False
+
+def eat_bad_but_not_sick(person, bad_cheese):
+    if bad_cheese in eat_cheese[person] and person not in sick_p:
+        return True
+    return False
     
 max_count = 0
 
-for i in range(1, M+1):
-    bad_cheeze = i
+for bad_cheese in range(1, M + 1):
     need_person = set()
-    for j in range(D):
-        if m[j] != bad_cheeze:
+    for person, cheese, time in zip(p, m, t):
+        # 지금 먹은게 상한 치즈 아니라명
+        if cheese != bad_cheese:
             continue
-        if p[j] not in need_person:
-            if fail_check_sick(p[j], t[j]):
-                break
-            need_person.add(p[j]) 
+        # 지금 먹은게 상한 치즈라면
+        # 1. 무조건 아파야 함, 2. 먹은 시간 < 아픈 시간 
+        # if 이미 받았는데 또 먹었을 경우 -> 시간 관계 없음
+        if person not in sick_p:
+            need_person.add(person)
+        for i in range(S):
+            if person == sick_p[i] and time < sick_t[i]:
+                need_person.add(person)
+        
             
+
+        
+        # 모순 1. sick & not eat bad
+        # 모순 2. sick & eat bad & late sick time
+        # 모순 3. eat bad & not sick
+      
     max_count = max(max_count, len(need_person))
+            
+
 
 print(max_count)
             
