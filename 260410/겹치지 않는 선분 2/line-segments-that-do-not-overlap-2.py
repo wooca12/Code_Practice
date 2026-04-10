092bf187741de658d3de688ffce02bf295744378
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-10 16:40:20 +0900

    Title: 겹치지 않는 선분 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260410/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 260410/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -1,13 +1,14 @@
 n = int(input())
 lines = [tuple(map(int, input().split())) for _ in range(n)]
 
-count = 0
+count = [1] * n
 for i in range(n):
-    for j in range(n):
-        if i == j:
+    for j in range(i+1, n):
+        if i == j :
             continue
         a1, a2 = lines[i]
         b1, b2 = lines[j]
-        if (a1 <= b1 and a2 >= b2) or (a1 >= b1 and a2 <= b2):
-            count += 1
-print(len(lines) - count)
+        if (a1 - b1) * (a2 - b2) <= 0:
+            count[i] = 0
+            count[j] = 0
+print(sum(count))
