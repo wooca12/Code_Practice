3e04d5e3fb22bf720572e6979cadac15be41a88e
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-11 17:41:20 +0900

    Title: 스승의 은혜 3, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260411/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -0,0 +1,23 @@
+N, B = map(int, input().split())
+gifts = [tuple(map(int, input().split())) for _ in range(N)]
+P = [gift[0] for gift in gifts]
+S = [gift[1] for gift in gifts]
+
+max_count = 0
+
+for i in range(N):
+    P[i] /= 2
+    price_list = [P[a] + S[a] for a in range(N)] 
+    price_list.sort()
+
+    price, count = 0, 0
+    
+    for j in range(N):
+        price += price_list[j]
+        if price <= B:
+            count += 1
+    max_count = max(max_count, count)
+
+print(max_count)
+        
+        
