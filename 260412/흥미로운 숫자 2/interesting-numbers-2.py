e3578b8b8d31567ce147c98ec11f503ea5298c60
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-12 01:26:08 +0900

    Title: 흥미로운 숫자 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260412/흥미로운 숫자 2/interesting-numbers-2.py
+++ 260412/흥미로운 숫자 2/interesting-numbers-2.py
@@ -1,25 +1,42 @@
 X, Y = map(int, input().split())
 
 
-            
 count = 0
-for num in range(X, Y + 1):
-    n_arr = [int(n) for n in str(num)]
-    
-    freq_num  = [0] * 10
-    two_num = []
+
+for num in range(X, Y+1):
+    n_arr = [int(i) for i in str(num)]
+
+    freq_num = [0] * 10
+    all_num = 0
+
     for n in n_arr:
         freq_num[n] += 1
-        if n not in two_num:
-            two_num.append(n)
+        all_num += 1
+    
+    interest = False
+    for i in range(10):
+        if freq_num[i] == all_num - 1:
+            interest = True
+    if interest:
+        count += 1
+        
+# count = 0
+# for num in range(X, Y + 1):
+#     n_arr = [int(n) for n in str(num)]
+    
+#     freq_num  = [0] * 10
+#     two_num = []
+#     for n in n_arr:
+#         freq_num[n] += 1
+#         if n not in two_num:
+#             two_num.append(n)
 
-    if len(two_num) != 2:
-        continue
-    if freq_num[two_num[0]] ==1 and freq_num[two_num[1]] >= 2:
-        count += 1
-    elif freq_num[two_num[0]] >= 2 and freq_num[two_num[1]] == 1:
-        count += 1
+#     if len(two_num) != 2:
+#         continue
+#     if freq_num[two_num[0]] ==1 and freq_num[two_num[1]] >= 2:
+#         count += 1
+#     elif freq_num[two_num[0]] >= 2 and freq_num[two_num[1]] == 1:
+#         count += 1
     
         
-
 print(count)
