fa833531f9f91d742bfbfb503809d47f00edc487
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-11 16:32:26 +0900

    Title: 이상한 폭탄 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260411/이상한 폭탄 2/strange-bomb-2.py
@@ -0,0 +1,20 @@
+N, K = map(int, input().split())
+num = [int(input()) for _ in range(N)]
+
+# 폭탄 개수 N
+# 특정 거리 K
+# 같은 번호 부여된 폭탄끼리 거리가 K 안에 있으면
+# return : 폭발 폭탄 중 가장 큰 번호
+
+max_num = 0
+
+for i in range(N):
+    bum = False
+    bum_num = []
+    for j in range(i+1, N):
+        if num[i] == num[j] and (j - i) <= K:
+            bum = True
+        if bum:
+           max_num = max(max_num, num[j])
+
+print(max_num)
