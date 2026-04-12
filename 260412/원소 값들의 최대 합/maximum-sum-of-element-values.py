10d97d0fb5414d776e9ff28289f080a8cc79dfd3
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-12 21:35:42 +0900

    Title: 원소 값들의 최대 합, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260412/원소 값들의 최대 합/maximum-sum-of-element-values.py
+++ 260412/원소 값들의 최대 합/maximum-sum-of-element-values.py
@@ -2,12 +2,12 @@ n, m = map(int, input().split())
 arr = [0] + list(map(int, input().split()))
 
 max_sum = 0
-for i in range(n):
+for i in range(1, n + 1):
     move = arr[i] 
     move_sum = 0
     for _ in range(m): # 2 3
         move_sum += arr[move] # 4 + 2
-        move = arr[move] - 1 # 3 1
+        move = arr[move] # 3 1
 
     max_sum = max(max_sum, move_sum)
 print(max_sum)
