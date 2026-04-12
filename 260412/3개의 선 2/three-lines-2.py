22fd1408336cd3e16d90f62f83556e0c5538e06d
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-12 13:29:46 +0900

    Title: 3개의 선 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260412/3개의 선 2/three-lines-2.py
+++ 260412/3개의 선 2/three-lines-2.py
@@ -1,25 +1,30 @@
+import sys
 n = int(input())
 points = [tuple(map(int, input().split())) for _ in range(n)]
 x, y = zip(*points)
 x, y = list(x), list(y)
 
-lines = []
+lines = set()
 for i in set(x):
-    lines.append([i, 0])
+    lines.add((i, 0))
 for i in set(y):
-    lines.append([0, i])
-
+    lines.add((0, i))
+lines = list(lines)
 ln = len(lines)
-ans = 0
+
 for i in range(ln):
     for j in range(i + 1, ln):
         for k in range(j + 1, ln):
+            ans = 1
             for x, y in points:
                 l = [lines[i], lines[j], lines[k]]
                 xs, ys = zip(*l)
                 xs, ys = list(xs), list(ys)
-                if x in xs or y in ys:
-                    ans = 1
+                if x not in xs and y not in ys:
+                    ans = 0
                     break
+            if ans:
+                print(ans)
+                sys.exit()
 print(ans)
-
+    
