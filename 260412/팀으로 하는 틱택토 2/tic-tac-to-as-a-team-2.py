f1f66b0569ace442983c00d2ffdafdbb083161b4
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-12 15:01:21 +0900

    Title: 팀으로 하는 틱택토 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260412/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 260412/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -1,25 +1,71 @@
 inp = [[int(n) for n in input()] for _ in range(3)]
 
-count = 0
-inp2 = [[inp[i][j] for i in range(3)] for j in range(3)]
-inp3 = [inp[i][i] for i in range(3)]
-inp4 = [inp[2-i][i] for i in range(3)]
-
-
 def win(p1, p2):
-    # 가로 / 세로
-    flag = True
-    a = set([p1, p2])
+    # 가로 
     for i in range(3):
-        if len(a ^ set(inp[i])) == 0:
+        c1, c2 = 0, 0
+        for j in range(3):
+            if p1 == inp[i][j]:
+                c1 += 1
+            elif p2 == inp[i][j]:
+                c2 += 1
+
+        if c1 + c2 == 3 and c1 > 0 and c2 > 0:
             return True
-        elif len(a ^ set(inp2[i])) == 0:
+
+    # 세로
+    for i in range(3):
+        c1, c2 = 0, 0
+        for j in range(3):
+            if p1 == inp[j][i]:
+                c1 += 1
+            elif p2 == inp[j][i]:
+                c2 += 1
+        if c1 + c2 == 3 and c1 > 0 and c2 > 0:
             return True
-    if len(a ^ set(inp3)) == 0:
-            return True
-    elif len(a ^ set(inp4)) == 0:
+
+    # 대각선 1
+    c1, c2 = 0, 0
+    for i in range(3):
+        if p1 == inp[i][i]:
+                c1 += 1
+        elif p2 == inp[i][i]:
+            c2 += 1
+    if c1 + c2 == 3 and c1 > 0 and c2 > 0:
         return True
+
+    # 대각선 2
+    c1, c2 = 0, 0
+    for i in range(3):
+        if p1 == inp[2-i][i]:
+                c1 += 1
+        elif p2 == inp[2-i][i]:
+            c2 += 1
+
+    if c1 + c2 == 3 and c1 > 0 and c2 > 0:
+        return True
+
     return False
+    
+    
+# inp2 = [[inp[i][j] for i in range(3)] for j in range(3)]
+# inp3 = [inp[i][i] for i in range(3)]
+# inp4 = [inp[2-i][i] for i in range(3)]
+
+# def win(p1, p2):
+#     # 가로 / 세로
+#     flag = True
+#     a = set([p1, p2])
+#     for i in range(3):
+#         if len(a ^ set(inp[i])) == 0:
+#             return True
+#         elif len(a ^ set(inp2[i])) == 0:
+#             return True
+#     if len(a ^ set(inp3)) == 0:
+#             return True
+#     elif len(a ^ set(inp4)) == 0:
+#         return True
+#     return False
         
             
         
