214424c272cda7887d2a1bdf6db149eedfc83ca2
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-14 19:02:23 +0900

    Title: 훌륭한 점프, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260414/훌륭한 점프/great-jump.py
+++ 260414/훌륭한 점프/great-jump.py
@@ -5,20 +5,20 @@ arr = list(map(int, input().split()))
 minmax = 100
 
 def is_possible(max_val):
-    available_indices = []
-    for i, elem in enumerate(arr):
+    available_indices = [0]
+    for i, elem in enumerate(arr[1:n-1]):
         if elem <= max_val:
             available_indices.append(i)
-            
+
+    available_indices.append(n-1)
+
     arr_size = len(available_indices)
+
     for i in range(1, arr_size):
         dist = available_indices[i] - available_indices[i-1]
         if dist > k:
             return False
 
-
-    if arr_size == 1:
-        return False
     return True
 
 
