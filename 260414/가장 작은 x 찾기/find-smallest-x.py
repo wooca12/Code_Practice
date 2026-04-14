7dba9d7b7b52ccfea77b4fde9716f485e615d1bd
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-14 17:05:56 +0900

    Title: 가장 작은 x 찾기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260414/가장 작은 x 찾기/find-smallest-x.py
+++ 260414/가장 작은 x 찾기/find-smallest-x.py
@@ -3,16 +3,17 @@ ranges = [tuple(map(int, input().split())) for _ in range(n)]
 a, b = zip(*ranges)
 a, b = list(a), list(b)
 
-min_x = 10
+min_x = 10001
 
 
-for x in range(1, 11):
+for x in range(1, 10001):
     success = True
     num  = x
     for i in range(n):
         num *= 2
         if a[i] > num or num > b[i]:
             success = False
+            break
     if success:
         min_x = min(min_x, x)
             
