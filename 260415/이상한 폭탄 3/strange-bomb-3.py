ecbd3a0e58d7a30457961b935900162b4b75a2e4
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-15 18:11:11 +0900

    Title: 이상한 폭탄 3, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260415/이상한 폭탄 3/strange-bomb-3.py
+++ 260415/이상한 폭탄 3/strange-bomb-3.py
@@ -7,14 +7,18 @@ set_num = set(num)
 
 for i in set_num:
     indices = []
+    # 같은 번호를 갔는 모든 인덱스
     for j in range(N):
         if i == num[j]:
             indices.append(j)
     size = len(indices)
+
+    # 인접한 거리가 K 이내 일때 터지는 횟수
     for j in range(size - 1):
         dist = indices[j+1] - indices[j]
         if dist > K:
             size -= 1
+    # 터진 횟수가 최대라면 번호와 횟수 갱신
     if size > 1 and size > max_size:
         max_n = i
         max_size = size
