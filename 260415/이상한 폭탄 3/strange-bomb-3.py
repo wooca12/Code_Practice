4e2a1e9206c7b5cecdea4c56cb33ef8122dbe350
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-15 18:03:53 +0900

    Title: 이상한 폭탄 3, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260415/이상한 폭탄 3/strange-bomb-3.py
+++ 260415/이상한 폭탄 3/strange-bomb-3.py
@@ -1,14 +1,22 @@
 N, K = map(int, input().split())
 num = [int(input()) for _ in range(N)]
 
-max_num = -1
-max_count = 0
-for i in range(N):
-    count = 1
-    for j in range(i+1, N):
-        if num[i] == num[j] and j - i <= N:
-            count += 1
-    if count >= max_count:
-        max_num = num[i]
-        max_count = count
-print(max_num)
+max_size = 0
+max_n = 0
+set_num = set(num)
+
+for i in set_num:
+    indices = []
+    for j in range(N):
+        if i == num[j]:
+            indices.append(j)
+    size = len(indices)
+    for j in range(size - 1):
+        dist = indices[j+1] - indices[j]
+        if dist > K:
+            size -= 1
+    if size > 1 and size > max_size:
+        max_n = i
+        max_size = size
+    
+print(max_n)
