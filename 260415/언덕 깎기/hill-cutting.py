6cb81c5ed41a14eca023b3242f1eed15d0e5d351
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-15 19:16:07 +0900

    Title: 언덕 깎기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260415/언덕 깎기/hill-cutting.py
+++ 260415/언덕 깎기/hill-cutting.py
@@ -1,19 +1,25 @@
 import sys
 N = int(input())
 heights = [int(input()) for _ in range(N)]
+k = 17
 
 MAX_H = 100
 mincost = sys.maxsize
 
-min_h = min(heights)
-max_h = max(heights)
-
-for i in range(1, 100):
-    for j in range(1, 100):
-        diff = (max_h - j) - (min_h + i)
-        if  0 <= diff <= 17:
-            cost = i*i + j*j
-            mincost = min(mincost, cost)
+# 크기가 17이 되는 모든 구간 잡기
+# 구간 안데 들어오게 언덕을 깍음
+for i in range(MAX_H):
+    # [i, i + 17] 구간에서 언덕을 깍는 비용 계산
+    # i + 17보다 높을때 : 언덕이 i + 17되도록 깎음
+    # i보다 낮을때 : 언덕이 i가 되도록 쌓음
+    cost = 0
+    for j in range(N):
+        if heights[j] < i:
+            cost += (i - heights[j]) ** 2
+        if heights[j] > i + 17:
+            cost += (i + k - heights[j]) ** 2
+    mincost = min(mincost, cost)
 print(mincost)
+        
         
     
