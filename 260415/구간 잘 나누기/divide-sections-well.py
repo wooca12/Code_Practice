611c4dab3ad3ea71d5d9e7962c5c4c3144665c34
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-15 19:50:50 +0900

    Title: 구간 잘 나누기, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260415/구간 잘 나누기/divide-sections-well.py
@@ -0,0 +1,30 @@
+import sys
+, m = map(int, input().split())
+a = list(map(int, input().split()))
+
+
+minmax = sys.maxsize
+MAX = 10000
+for sum in range(1, MAX + 1):
+    # 구간의 합의 최대가 sum일 때
+
+    possible = True
+    section = 1
+
+    count = 0
+    for j in range(n):
+        # 해당 숫자가 구간의 합의 최대값보다 크면 안됨
+        if a[j] > sum:
+            possible = False
+            break
+        # 구간에 해당 숫자가 더 해졌을때 최대값보다 큼 -> 해당 숫자를 다음 구간으로 만듦
+        if count + a[j] > sum:
+            count = 0
+            section += 1
+        # 구간에 해당 숫자를 집어 넣는다
+        count += a[j]
+
+        if possible and section <= m:
+            minmax = min(minmax, sum)
+print(minmax)
+
