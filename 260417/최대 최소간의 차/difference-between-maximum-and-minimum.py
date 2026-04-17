e7246d2dcdad0b45a8925914663e0f072a5409ea
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-17 16:12:22 +0900

    Title: 최대 최소간의 차, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260417/최대 최소간의 차/difference-between-maximum-and-minimum.py
@@ -0,0 +1,19 @@
+import sys
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+MAX = 10000
+
+mincost = sys.maxsize
+
+for i in range(1, MAX):
+    cost = 0
+    for j in range(n):
+        if  arr[j] < i:
+            cost += abs(arr[j] - i)
+        elif arr[j] > i + k:
+            cost += abs(arr[j] - i - k)
+    mincost = min(mincost, cost)
+
+print(mincost)
+
