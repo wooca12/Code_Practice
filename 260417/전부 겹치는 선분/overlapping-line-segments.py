47969e94a883fb6d6f836f87cb26d89689e17654
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-17 17:02:24 +0900

    Title: 전부 겹치는 선분, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260417/전부 겹치는 선분/overlapping-line-segments.py
+++ 260417/전부 겹치는 선분/overlapping-line-segments.py
@@ -3,13 +3,22 @@ segments = [tuple(map(int, input().split())) for _ in range(n)]
 x1, x2 = zip(*segments)
 x1, x2 = list(x1), list(x2)
 
-ans = 'No'
-for x in range(min(x1), max(x2) + 1):
-    count = 0
-    for a, b in segments:
-        if a <= x <= b:
-            count += 1
-    if count == n:
-        ans = 'Yes'
-        break
-print(ans)
+
+max_a = max(x1)
+min_b = min(x2)
+
+# 시작점 a가 끝점 b보다 뒤에 온다면 선분 겹침  
+if max_a <= min_b:
+    print('Yes')
+else:
+    print('No')
+# ans = 'No'
+# for x in range(min(x1), max(x2) + 1):
+#     count = 0
+#     for a, b in segments:
+#         if a <= x <= b:
+#             count += 1
+#     if count == n:
+#         ans = 'Yes'
+#         break
+# print(ans)
