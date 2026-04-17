0f8f5cee3d1ff99c3028517eac89d68856885c04
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-17 16:47:26 +0900

    Title: 구역 청소, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260417/구역 청소/district-cleaning.py
+++ 260417/구역 청소/district-cleaning.py
@@ -1,20 +1,31 @@
 a, b = map(int, input().split())
 c, d = map(int, input().split())
 
-# 안겹칠때
-if d <= a or b <= c:
-    area = abs(b - a) + abs(d - c)
-# 겹칠때 1. B구역이 A 구역에 포함
-elif (a < c < d < b) :
-    area = abs(b - a)
-elif (c < a < b < d):
-    area = abs(d - c)
-# 겹칠때 2. 걸쳐 있을때 
-elif a < c < b < d :
-    area = abs(b - a) + abs(d - b)
-elif c < a < d < b : 
-    area = abs(b - a) + abs(a - c)
-else:
-    area = abs(b - a)
 
-print(area)
+def intersecting(x1, x2, x3, x4):
+    if x2 < x3 or x4 < x1:
+        return False
+    else:
+        return True
+
+if intersecting(a,b,c,d):
+    print(max(b, d) - min(a, c))
+else:
+    print((b-a) + (d-c))
+
+# # 안겹칠때
+# if d <= a or b <= c:
+#     area = abs(b - a) + abs(d - c)
+# # 겹칠때 1. B구역이 A 구역에 포함
+# elif (a < c < d < b) :
+#     area = abs(b - a)
+# elif (c < a < b < d):
+#     area = abs(d - c)
+# # 겹칠때 2. 걸쳐 있을때 
+# elif a < c < b < d :
+#     area = abs(b - a) + abs(d - b)
+# elif c < a < d < b : 
+#     area = abs(b - a) + abs(a - c)
+# else:
+#     area = abs(b - a)
+# print(area)
