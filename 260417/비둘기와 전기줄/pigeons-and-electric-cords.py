7e19b09b8bea4f8cef344320352dac0e0aa10ae8
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-17 19:37:13 +0900

    Title: 비둘기와 전기줄, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260417/비둘기와 전기줄/pigeons-and-electric-cords.py
@@ -0,0 +1,17 @@
+N = int(input())
+pigeon_pos = [-1] * N
+moves = []
+count = 0
+for _ in range(N):
+    p, pos = map(int, input().split())
+    moves.append((p, pos))
+
+count = 0
+for p, pos in moves:
+    if pigeon_pos[p] == -1:
+        pigeon_pos[p] = pos
+    elif pigeon_pos[p] != pos:
+        count += 1
+print(count)
+    
+
