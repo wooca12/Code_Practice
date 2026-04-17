e20bff53a0138609604beef438c4bcf7027fdda5
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-17 20:49:12 +0900

    Title: 코딩톡, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260417/코딩톡/coding-talk.py
+++ 260417/코딩톡/coding-talk.py
@@ -1,20 +1,24 @@
+import sys
 n, m, p = map(int, input().split())
 messages = [tuple(input().split()) for _ in range(m)]
 c = [msg[0] for msg in messages]
 u = [int(msg[1]) for msg in messages]
 
 # n명, m개 메시지, p번째 문자
-all_person = [chr(i) for i in range(ord('A'), ord('A')+ n)]
+if u[p-1] == 0:
+    sys.exit()
 
-read = []
-for i in range(p-1, n):
-    person = c[i]
-    if person not in read:
-        read.append(person)
-num = u[p-1]
+for i in range(n):
+    person = chr(ord('A') + i)
+    read = False
 
-for p in all_person:
-    if num > 0 and p not in read:
-        print(p, end=' ')
+    for j in range(m):
+        # message 보낸사람이 맞음 + p 메시지 읽은 수와 크거나 같음 
+        if c[j] == person and u[j] >= u[p-1]:
+            read = True
+
+    if read == False:
+        print(person, end = ' ')
+
     
         
