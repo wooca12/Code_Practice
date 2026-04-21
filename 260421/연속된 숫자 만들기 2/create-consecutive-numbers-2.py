0c5f59bd569d843ca03a9744e1e6f99e028da465
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-21 17:06:21 +0900

    Title: 연속된 숫자 만들기 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260421/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 260421/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -3,8 +3,6 @@ pos.sort()
 ans = 0
 if pos[0] + 1 == pos[1] and pos[1] + 1 == pos[2]:
     ans = 0
-elif pos[0] + 1 == pos[1] or pos[1] + 1 == pos[2]:
-    ans = 1
 elif pos[0] + 2 == pos[1] or pos[1] + 2 == pos[2]:
     ans = 1
 else:
