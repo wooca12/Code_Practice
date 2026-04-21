7b4062cc14e14f79625a2a893b7acae29fcf2726
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-21 17:52:23 +0900

    Title: 독서실의 거리두기 3, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260421/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
+++ 260421/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
@@ -1,21 +1,51 @@
 N = int(input())
 seats = [int(i) for i in input()]
-maxmin = 0
 
-for i in range(N):
-    if seats[i] == 1:
-        continue
+# 사용중인 좌석 번호 구함
+def get_pos(seats):
+    pos_indices = []
+    for i in range(N):
+        if seats[i] == 1:
+            pos_indices.append(i)
+    return pos_indices
 
-    seats[i] = 1
-    pos = [i for i in range(N) if seats[i] == 1]
+# 좌석 간 가장 거리가 넓은 좌석 1쌍
+max_i, max_j= 0, 0
+max_diff = 0
+pos = get_pos(seats)
+for i in range(1, len(pos)):
+    if pos[i] - pos[i-1] > max_diff :
+        max_diff = pos[i] - pos[i-1]
+        max_i, max_j = pos[i-1], pos[i]
 
-    min_diff = 10000
-    for j in range(1, len(pos)):
-        min_diff = min(min_diff, abs(pos[j] - pos[j-1]))
+# 넓은 좌석 중간에 1 놓음
+seats[(max_i + max_j) //2 ] = 1
+new_pos = get_pos(seats)
 
-    maxmin = max(maxmin, min_diff)
+# 좌석 간 가장 거리가 가까운 좌석 1쌍
+ans = 1000
+for i in range(1, len(new_pos)):
+    ans = min(ans, new_pos[i] - new_pos[i-1]) 
 
-    seats[i] = 0
+print(ans)
+
+####################################################
+
+# maxmin = 0
+# for i in range(N):
+#     if seats[i] == 1:
+#         continue
+
+#     seats[i] = 1
+#     pos = [i for i in range(N) if seats[i] == 1]
+
+#     min_diff = 10000
+#     for j in range(1, len(pos)):
+#         min_diff = min(min_diff, abs(pos[j] - pos[j-1]))
+
+#     maxmin = max(maxmin, min_diff)
+
+#     seats[i] = 0
 
 
-print(maxmin)
+# print(maxmin)
