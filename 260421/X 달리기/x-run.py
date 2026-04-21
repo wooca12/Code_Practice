775a812c39b801f8c5bdf85ad3b8253636a825a7
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-21 15:48:46 +0900

    Title: X 달리기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260421/X 달리기/x-run.py
@@ -0,0 +1,26 @@
+import sys
+X = int(input())
+
+min_time = 10000
+for point in range(X):
+    dist = 0
+    speed = 0
+    for t in range(10000):
+        dist += speed
+        if point == 6:
+            print(point, speed, dist, t)
+
+        if speed < 0:
+            break
+        if dist == X and speed == 1:
+            min_time = min(min_time, t)
+            break
+        if dist < point:
+            speed += 1
+        elif dist >= point and speed != 1:
+            speed -= 1
+
+        
+
+
+print(min_time)
