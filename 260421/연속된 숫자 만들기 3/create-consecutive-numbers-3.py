7fdc9c3c053fd8cf3ced2add842a06ac4e3dd177
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-21 21:02:52 +0900

    Title: 연속된 숫자 만들기 3, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260421/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
+++ 260421/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
@@ -1,4 +1,12 @@
 a = list(map(int, input().split()))
-cnt = max(a[1] - a[0], a[2] - a[1]) - 1
+maxmove = 0
 
-print(cnt)
+# 움직일 필요 없음
+if a[0] + 1 == a[1] and a[1] + 1 == a[2]:
+    maxmove = 0
+
+# 오른쪽 끝 사람이 가장 먼저 이동: 최대 횟수 (0번~1번 거리 - 1)회
+maxmove = max(maxmove, a[1] - a[0] - 1)
+# 왼쪽 끝 사람이 가장 먼저 이동: 최대 횟수 (1~2번 거리 - 1)회
+maxmove = max(maxmove, a[2] - a[1] - 1)
+print(maxmove)
