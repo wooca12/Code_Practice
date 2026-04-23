7e7fd93c48d7d5dece2de830fa68b56ab9373179
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-23 18:51:33 +0900

    Title: 두 번째로 작은 수의 위치, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260423/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.py
+++ 260423/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.py
@@ -5,15 +5,23 @@ a = [0] + list(map(int, input().split()))
 min_n = min(a[1:])
 min_diff = sys.maxsize
 ans = -1
+cnt = 0
 
 for i in range(1, n+1):
     if a[i] == min_n:
         continue
-    # 최소 간격 2개 이상
-    elif a[i] - min_n == min_diff:
-        ans = -1
-        break
+    # 더 간격이 작은 숫자 등장 시
     elif a[i] - min_n < min_diff:
         ans = i
+        cnt = 1
         min_diff = a[i] - min_n
-print(ans)
+    # 간격이 똑같은 숫자 등장 시
+    elif a[i] - min_n == min_diff:
+        cnt += 1
+
+
+# 등장 횟수 0 또는 2 이상
+if cnt == 0 or cnt > 1:
+    print(-1)
+else:
+    print(ans)
