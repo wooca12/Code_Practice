2a8e299b98ae775ca7beaab8521d8716a7cc82a7
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-23 17:17:47 +0900

    Title: 독서실의 거리두기 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260423/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
+++ 260423/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -5,26 +5,30 @@ max_cnt = 0
 max_i, max_j = 0, 0
 for i in range(N):
     for j in range(i+1, N):
-        if seats[i] != '1' or seats[j] != '1':
+        if seats[j] != '1' or seats[j] != '1':
             continue
-        if max_cnt < j - i:
+        if j - i > max_cnt:
             max_cnt = j - i
             max_i, max_j = i, j
         break
-
 mid = (max_i + max_j) // 2
-min_cnt = min(mid - max_i, max_j - mid)
+
+def get_min(index):
+    seats[index] = '1'
+    mincnt = N
+    for i in range(N):
+        for j in range(i+1, N):
+            if seats[i] == '1' and seats[j] == '1':
+                mincnt = min(mincnt, j - i)
+    seats[index] = '0'
+    return mincnt
+
+maxmin = get_min(mid)
+
 
 
 if seats[0] == '0':
-    for i in range(N):
-        if seats[i] == '1':
-            min_cnt = max(min_cnt, i - 0)
-            break
+    maxmin = max(maxmin, get_min(0))
 if seats[N-1] == '0':
-    for i in range(N-1, -1, -1):
-        if seats[i] == '1':
-            min_cnt = max(min_cnt, N - 1 - i)
-            break
-print(min_cnt)
-
+    maxmin = max(maxmin, get_min(N-1))
+print(maxmin)
