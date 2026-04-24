78db3ad784744223d3d9e0fcb59e48a94b1ece71
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-24 14:39:59 +0900

    Title: 최소 와이파이 수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260424/최소 와이파이 수/minimum-number-of-wifi.py
+++ 260424/최소 와이파이 수/minimum-number-of-wifi.py
@@ -2,16 +2,13 @@ import math
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 
-# 거리가 0 일때 : 사람 사는 곳마다 있어야 함
-if m == 0:
-    num = sum(arr)
+cnt, i = 0, 0
+while i < n:
+    if arr[i] == 1:
+        cnt += 1
+        i += 2 * m + 1
+    else:
+        i += 1
 
-# 사람이 없을 때 : 설치할 필요 없음
-elif sum(arr) == 0:
-    num = 0
-# 거리가 0 이상, 사람 1 이상
-else:
-    num = n / (m * 2 + 1)
-    num = math.ceil(num)
-print(num)
+print(cnt)
     
