3e79270ebc5bfe28ca6b23bc090cd6355b135684
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-24 14:26:04 +0900

    Title: 최소 와이파이 수, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260424/최소 와이파이 수/minimum-number-of-wifi.py
+++ 260424/최소 와이파이 수/minimum-number-of-wifi.py
@@ -2,7 +2,10 @@ import math
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 
-num = n / (m * 2 + 1)
-num = math.ceil(num)
+if m == 0:
+    num = sum(arr)
+else:
+    num = n / (m * 2 + 1)
+    num = math.ceil(num)
 print(num)
     
