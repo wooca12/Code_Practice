74dc4fef0d06d97bef8912b70206803f9e785295
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-04-29 19:41:39 +0900

    Title: 사칙연산 연습, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260429/사칙연산 연습/practice-basic-arithmetic-operations.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 30;
+    int c = a + b;
+    cout << c / 8;
+    cout << "\n";
+    cout << c % 8;
+    return 0;
+}
