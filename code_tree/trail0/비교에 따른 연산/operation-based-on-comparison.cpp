e05678a034fa48653646de1cd3c4643d153f7750
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-14 19:10:28 +0900

    [Passed] 비교에 따른 연산 (8ms, 0MB)

--- code_tree/trail0/비교에 따른 연산/operation-based-on-comparison.cpp
+++ code_tree/trail0/비교에 따른 연산/operation-based-on-comparison.cpp
@@ -5,8 +5,10 @@ int main() {
     int a, b;
     cin >> a >> b;
 
-    if (a > b) cout << a * b << endl;
-    else cout << b / a << endl;
+    if (a > b) 
+        cout << a * b << endl;
+    else 
+        cout << b / a << endl;
 
     return 0;
 }
