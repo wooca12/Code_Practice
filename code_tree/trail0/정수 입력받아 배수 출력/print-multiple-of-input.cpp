9f9bc17dab8fd0694d6227261ec0da016033fd21
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-14 20:11:12 +0900

    [Passed] 정수 입력받아 배수 출력 (9ms, 0MB)

---
+++ code_tree/trail0/정수 입력받아 배수 출력/print-multiple-of-input.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    int i;
+    for (i = 1; i < 6; i++) {
+        cout << n * i << " ";
+    }
+
+    return 0;
+}
