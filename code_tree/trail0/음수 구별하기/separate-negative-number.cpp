0ca2f3b99c4abdfafa3efe945bed513da9bdaade
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-14 18:50:48 +0900

    [Passed] 음수 구별하기 (8ms, 0MB)

---
+++ code_tree/trail0/음수 구별하기/separate-negative-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    cout << n << "\n";
+
+    if (n < 0) {
+        cout << "minus" << endl;
+    }
+
+    return 0;
+}   
