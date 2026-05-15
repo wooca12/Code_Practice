d002d85780b9b4c2fd7bfda1e9d9a0da90c504aa
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-15 20:53:11 +0900

    [Passed] 입력받은 값 교체하기 (7ms, 0MB)

---
+++ code_tree/trail1/입력받은 값 교체하기/changing-inputs.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, tmp;
+
+    cin >> a >> b;
+    tmp = a;
+    a = b;
+    b = tmp;
+    cout << a << " " << b << endl;
+
+    return 0;
+}
