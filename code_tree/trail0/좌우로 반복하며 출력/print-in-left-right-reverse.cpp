8866c2dccd11728e6c921ab368849c33e927634c
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-15 19:46:44 +0900

    [Passed] 좌우로 반복하며 출력 (8ms, 0MB)

---
+++ code_tree/trail0/좌우로 반복하며 출력/print-in-left-right-reverse.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, a;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i % 2 == 0) 
+                cout << j + 1;
+            else 
+                cout << n - j;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
