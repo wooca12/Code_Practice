3f6dbc96f5381b27b962b6776e1d938c46511aca
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-15 21:12:47 +0900

    [Passed] 날짜 변경하여 출력 (8ms, 0MB)

---
+++ code_tree/trail1/날짜 변경하여 출력/print-date-with-different-format.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int y, m, d;
+    char c;
+    cin >> y >> c >> m >> c >> d;
+    cout << m << "-" << d << "-" << y << endl;
+    return 0;
+}
