3920d55a40606659a7e587ad9a354812faa9c536
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-15 21:14:31 +0900

    [Passed] 전화번호 바꾸기 (9ms, 0MB)

---
+++ code_tree/trail1/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    char m;
+    cin >> a >> m >> b >> m >> c;
+    cout << 0 << a << "-" << c << "-" << b;
+    return 0;
+}
