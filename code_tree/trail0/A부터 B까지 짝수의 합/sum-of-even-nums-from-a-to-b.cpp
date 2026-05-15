82dd5dadd0b136e6c3bdc650b1fdb4fb0e3aa726
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-15 14:52:33 +0900

    [Passed] A부터 B까지 짝수의 합 (8ms, 0MB)

---
+++ code_tree/trail0/A부터 B까지 짝수의 합/sum-of-even-nums-from-a-to-b.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    int total = 0;
+
+    for (int i = a; i <= b; i++) {
+        if (i % 2 == 0)
+            total += i;
+    }
+    cout << total << endl;
+    
+    return 0;
+}
