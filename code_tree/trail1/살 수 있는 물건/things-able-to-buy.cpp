cf50523c39582eb3b69090b5b5f75029c1231237
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-16 17:38:19 +0900

    [Passed] 살 수 있는 물건 (8ms, 0MB)

---
+++ code_tree/trail1/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    if (N >= 3000)
+        cout << "book" << endl;
+    else if (N >= 1000)
+        cout << "mask" << endl;
+    else
+        cout << "no" << endl;
+    return 0;
+}
