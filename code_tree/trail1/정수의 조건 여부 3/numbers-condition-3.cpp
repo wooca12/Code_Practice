7ae9c36aad022a6ce8df3d867d923d16b5a81c93
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-16 18:37:11 +0900

    [Passed] 정수의 조건 여부 3 (8ms, 0MB)

---
+++ code_tree/trail1/정수의 조건 여부 3/numbers-condition-3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    if ((a % 13 == 0) || (a % 19 == 0))
+        cout << "True" << endl;
+    else
+        cout << "False" << endl;
+    return 0;
+}
