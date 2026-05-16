03d87f297df5bcbac3a2c97e6c30b10832d5cfe4
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-16 18:38:04 +0900

    [Passed] 둘 중 하나의 배수 (8ms, 0MB)

---
+++ code_tree/trail1/둘 중 하나의 배수/multiple-of-either.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    if ((a % 3 == 0) || (a % 5 == 0))
+        cout << 1 << endl;
+    else
+        cout << 0 << endl;
+    return 0;
+}
