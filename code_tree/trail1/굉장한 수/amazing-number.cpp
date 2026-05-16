44b9252fa290b55b2c35128d37909ba6f77a7ad6
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-16 18:42:06 +0900

    [Passed] 굉장한 수 (8ms, 0MB)

---
+++ code_tree/trail1/굉장한 수/amazing-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (((n % 2 == 1) && (n % 3 == 0)) || ((n % 2 == 0) && (n % 5 == 0)))
+        cout << "true" << endl;
+    else 
+        cout << "false" << endl;
+    return 0;
+}
