f6797f6027a76c7a223b9fb0a68a9bf260de1154
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-16 19:53:18 +0900

    [Passed] 3의 배수 출력하기 (8ms, 0MB)

---
+++ code_tree/trail1/3의 배수 출력하기/print-multiple-of-number-three.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, i = 1;  
+    cin >> n;
+
+    while (i <= n) {
+        if (i % 3 == 0)
+            cout << i << " ";
+        i++;
+    }
+    return 0;
+}
