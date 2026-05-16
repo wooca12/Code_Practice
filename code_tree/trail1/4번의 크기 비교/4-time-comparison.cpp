f05ceaecfb2887395d53e7cbe94d11a99513bb46
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-16 18:04:48 +0900

    [Passed] 4번의 크기 비교 (8ms, 0MB)

---
+++ code_tree/trail1/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, d, e;
+    cin >> a >> b >> c >> d >> e;
+
+    cout << (a > b ? 1 : 0) << endl;
+    cout << (a > c ? 1 : 0) << endl;
+    cout << (a > d ? 1 : 0) << endl;
+    cout << (a > e ? 1 : 0) << endl;
+    return 0;
+}
