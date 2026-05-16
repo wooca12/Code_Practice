066208bf4d865e5c515d8ad67188d89cfaf1aa7a
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-16 21:25:24 +0900

    [Passed] 5의 배수의 합 (8ms, 0MB)

---
+++ code_tree/trail1/5의 배수의 합/sum-of-specific-condition-multiple.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, sum = 0;
+    cin >> a >> b;
+
+    if (a <= b) {
+        for (int i = a; i <= b; i++) {
+            if (i % 5 == 0)
+                sum += i;
+        }
+    }
+    else {
+        for (int i = b; i <= a; i++) {
+            if (i % 5 == 0)
+                sum += i;
+        }
+    }
+    cout << sum << endl;
+    return 0;
+}
