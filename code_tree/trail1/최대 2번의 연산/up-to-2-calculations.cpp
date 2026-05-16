99d2a1dcf4d708491d0d5cad5d3156188af85fbf
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-16 17:48:36 +0900

    [Passed] 최대 2번의 연산 (8ms, 0MB)

---
+++ code_tree/trail1/최대 2번의 연산/up-to-2-calculations.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    if (a % 2 == 0)
+        a /= 2;
+    
+    if (a % 2 == 1)
+        a = (a + 1) / 2;
+    
+    cout << a << endl;
+
+    return 0;
+}
