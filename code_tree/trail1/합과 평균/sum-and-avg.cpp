27e7e20fa17210055b337677cfc6270d1f978a1b
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-16 15:10:28 +0900

    [Passed] 합과 평균 (8ms, 0MB)

---
+++ code_tree/trail1/합과 평균/sum-and-avg.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    cout << fixed;
+    cout.precision(1);
+
+    cout << a + b << " " << (double)(a + b) / 2 << endl;
+    return 0;
+}
