8dce42b8693ff9975242c8a247af165b7f0a1034
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-16 20:44:49 +0900

    [Passed] A 부터 B 까지 (8ms, 0MB)

---
+++ code_tree/trail1/A 부터 B 까지/a-to-b.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, i;
+    cin >> a >> b;
+
+    i = a;
+    while (i <= b) {
+        cout << i << " ";
+        if (i % 2 == 1)
+            i *= 2;
+        else
+            i += 3;
+
+    }
+
+    return 0;
+}
