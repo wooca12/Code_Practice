8066b7a2299268eef60f67de372ce3be0422b645
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-16 15:00:49 +0900

    [Passed] 길이 변형 후 사각형 넓이 구하기 (8ms, 0MB)

---
+++ code_tree/trail1/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    a += 8;
+    b *= 3;
+    
+    cout << a << endl;
+    cout << b << endl;
+    cout << a * b << endl;
+    return 0;
+}
