0766cf56c1a81f685a1967183b6d92860dae9eba
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-16 18:07:01 +0900

    [Passed] 점수 비교 (8ms, 0MB)

---
+++ code_tree/trail1/점수 비교/score-comparison.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a1, a2, b1, b2;
+    cin >> a1 >> a2;
+    cin >> b1 >> b2;
+
+    if (a1 > b1 && a2 > b2)
+        cout << 1 << endl;
+    else 
+        cout << 0 << endl;
+    return 0;
+}
