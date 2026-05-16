a05ad1e2b5891ae54bdc15e53e2d4b17f4235d50
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-16 20:54:47 +0900

    [Passed] 369 게임 (8ms, 0MB)

---
+++ code_tree/trail1/369 게임/369-game.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i ++) {
+        if (i % 3 == 0)
+            cout << 0 << " ";
+        else if (i > 10) {
+            if ((i % 10) != 0 && (i % 10) % 3 == 0)
+                cout << 0 << " ";
+            else if ((i / 10) != 0 && (i / 10) % 3 == 0)
+                cout << 0 << " ";
+            else 
+                cout << i << " ";
+        }
+        else 
+            cout << i << " ";
+    }
+    return 0;
+}
