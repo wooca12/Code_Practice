152da3b3aaaec00bde475037740e4b5c8d66e96d
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-16 19:50:05 +0900

    [Passed] 별 N개 출력하기 (7ms, 0MB)

---
+++ code_tree/trail1/별 N개 출력하기/print-n-stars.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int i = 0;
+    while (i < n) {
+        cout << "*" << endl;
+        i++;
+    }
+    return 0;
+}
