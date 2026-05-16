87e9469e4d73c90810f09937eec0cd03bb63132a
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-16 17:30:31 +0900

    [Passed] 2개 중 최대 (8ms, 0MB)

---
+++ code_tree/trail1/2개 중 최대/max-of-two-nums.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+
+    int c = a > b ? a : b;
+
+    cout << c << endl;
+    return 0;
+}
