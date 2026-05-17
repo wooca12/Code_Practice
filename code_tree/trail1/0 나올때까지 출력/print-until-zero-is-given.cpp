8020022c983192888ca68de881badb1f823188ab
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-17 18:44:17 +0900

    [Passed] 0 나올때까지 출력 (8ms, 0MB)

---
+++ code_tree/trail1/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (;;) {
+        int n;
+        cin >> n;
+        if (n == 0)
+            break;
+        cout << n << endl;
+    }
+    return 0;
+}
