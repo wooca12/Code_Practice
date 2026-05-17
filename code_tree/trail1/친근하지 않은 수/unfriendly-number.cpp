a25c803ed20cd87bd1eb717bcaf5f52a56536c86
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-17 17:59:19 +0900

    [Passed] 친근하지 않은 수 (8ms, 0MB)

---
+++ code_tree/trail1/친근하지 않은 수/unfriendly-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, cnt = 0;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        if ((i % 2 == 0) || (i % 3 == 0) || (i % 5 == 0))
+            continue;
+        else   
+            cnt++;
+    }
+    cout << cnt << endl; 
+    return 0;
+}
