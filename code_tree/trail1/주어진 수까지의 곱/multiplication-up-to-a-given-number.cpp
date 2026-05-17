a6263fb4e9e005526051c9e5308e3060a1320bb0
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-17 17:54:13 +0900

    [Passed] 주어진 수까지의 곱 (8ms, 0MB)

---
+++ code_tree/trail1/주어진 수까지의 곱/multiplication-up-to-a-given-number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, prod = 1;
+    cin >> a >> b;
+    for (int i = a; i <= b; i++) {
+        prod *= i;
+    }
+    cout << prod << endl;
+    return 0;
+}
