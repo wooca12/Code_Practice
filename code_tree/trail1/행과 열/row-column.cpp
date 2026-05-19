4488169895e99c21824273bd3b2e0068eff3db07
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-19 17:03:26 +0900

    [Passed] 행과 열 (1ms, 0MB)

---
+++ code_tree/trail1/행과 열/row-column.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    for (int i = 1; i <= a ; i++) {
+        for (int j = 1; j <= b; j++) {
+            cout << i * j << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
