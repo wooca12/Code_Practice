865ad6b3fc4d854ae4787506a4c5847f3310cba6
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-19 17:30:33 +0900

    [Passed] 행에 따라 다르게 출력 (2ms, 0MB)

---
+++ code_tree/trail1/행에 따라 다르게 출력/output-defferently-each-row.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, cnt = 0;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i % 2 == 0)
+                cnt++;
+            else
+                cnt += 2;
+            cout << cnt << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
