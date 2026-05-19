92ddc9cb946e3d2d30676fb790b00edfb089c7aa
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-19 17:14:08 +0900

    [Passed] 1-9 정사각형 (2ms, 0MB)

---
+++ code_tree/trail1/1-9 정사각형/1-9-square.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, cnt = 1;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (cnt >= 10)
+                cnt = 1;
+            cout << cnt;
+            cnt++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
