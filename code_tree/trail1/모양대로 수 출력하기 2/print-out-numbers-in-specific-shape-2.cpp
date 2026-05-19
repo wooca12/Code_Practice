3dca50a9216d3f706ef8348be1d86bc951e335b6
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-19 17:16:07 +0900

    [Passed] 모양대로 수 출력하기 2 (1ms, 0MB)

---
+++ code_tree/trail1/모양대로 수 출력하기 2/print-out-numbers-in-specific-shape-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, cnt = 2;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (cnt >= 10)
+                cnt = 2;
+            cout << cnt << " ";
+            cnt += 2;
+        }
+        cout << endl;
+    }
+    return 0;
+}
