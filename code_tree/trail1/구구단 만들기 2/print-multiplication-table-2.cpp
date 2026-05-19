06f3e1c84fa0e02619670a64984de778c0323ea4
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-19 17:40:12 +0900

    [Passed] 구구단 만들기 2 (1ms, 0MB)

---
+++ code_tree/trail1/구구단 만들기 2/print-multiplication-table-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    for (int i = 2; i < 10; i += 2) {
+        for (int j = b; j >= a; j--) {
+            cout << j << " * " << i << " = " << i * j;
+            if (j > a)
+                cout << " / ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
