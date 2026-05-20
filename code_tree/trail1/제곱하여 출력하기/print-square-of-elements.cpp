767bccfbe3ead7bc82199b293ae75a2fbff08745
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-21 01:26:52 +0900

    [Passed] 제곱하여 출력하기 (2ms, 0MB)

---
+++ code_tree/trail1/제곱하여 출력하기/print-square-of-elements.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[100];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        cout << arr[i] * arr[i] << " ";
+    }
+    return 0;
+}
