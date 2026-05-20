e92e269713f6ad7e325e1fe12a9c153d17feceb2
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-21 01:39:13 +0900

    [Passed] 특정 조건에 맞게 출력하기 (2ms, 0MB)

---
+++ code_tree/trail1/특정 조건에 맞게 출력하기/print-in-specific-conditions.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[100];
+
+    for (int i = 0; i < 100; i++) {
+        cin >> arr[i];
+        
+        if (arr[i] == 0)
+            break;
+        if (arr[i] % 2 == 0)
+            cout << arr[i] / 2 << " ";
+        else
+            cout << arr[i] + 3 << " ";
+    }
+    return 0;
+}
