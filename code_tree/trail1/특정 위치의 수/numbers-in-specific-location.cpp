e4c3a9872b5b810e7bacf6ba1c6325c2430cf014
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-21 00:59:24 +0900

    [Passed] 특정 위치의 수 (2ms, 0MB)

---
+++ code_tree/trail1/특정 위치의 수/numbers-in-specific-location.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10];
+    for (int i = 0; i < 10; i++) {
+        cin >> arr[i];
+    }
+    cout << arr[2] + arr[4] + arr[9] << endl;
+    return 0;
+}
