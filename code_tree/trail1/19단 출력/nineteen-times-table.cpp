b3b356f59c80765e81173b7d3ab911267dae49e7
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-20 23:36:35 +0900

    [Passed] 19단 출력 (3ms, 0MB)

---
+++ code_tree/trail1/19단 출력/nineteen-times-table.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    for (int i = 1; i < 20; i++) {
+        for (int j = 1; j < 20; j++) {
+            cout << i << " * " << j << " = " << i * j;
+            if ((j % 2 == 0) || j == 19)
+                cout << endl;
+            else
+                cout << " / ";
+        }
+    }
+    return 0;
+}
