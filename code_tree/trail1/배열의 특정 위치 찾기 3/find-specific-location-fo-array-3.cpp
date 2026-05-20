42245fedc9f271ca99d8b89f422f3008b8dca90d
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-21 01:06:43 +0900

    [Passed] 배열의 특정 위치 찾기 3 (2ms, 0MB)

---
+++ code_tree/trail1/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[100];
+
+    for (int i = 0; i < 100; i++) {
+        cin >> arr[i];
+        if (arr[i] == 0) {
+            cout << arr[i - 1] + arr[i - 2] + arr[i - 3] << endl;
+            break;
+        }
+    }
+    return 0;
+}
