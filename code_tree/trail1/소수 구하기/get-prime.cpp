7e5e9040dcfba19d8173e04588956e880a2e571b
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-21 00:01:33 +0900

    [Passed] 소수 구하기 (2ms, 0MB)

---
+++ code_tree/trail1/소수 구하기/get-prime.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 2; i <= n; i++) {
+        int cnt = 0;
+        for (int j = 1; j <= i; j++) {
+            if (i % j == 0) {
+                cnt++;
+            }
+        }
+        if (cnt == 2)
+            cout << i << " ";
+    }
+    return 0;
+}
