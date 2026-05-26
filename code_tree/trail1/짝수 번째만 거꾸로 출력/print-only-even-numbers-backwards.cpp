f754847c26a4d4f822ac6f05b39f62c82e1f83ff
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-27 00:18:17 +0900

    [Passed] 짝수 번째만 거꾸로 출력 (2ms, 0MB)

---
+++ code_tree/trail1/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+
+    for (int i = str.length() - 1; i >= 0; i--) {
+        if ((i + 1) % 2 == 0) {
+            cout << str[i];
+        }
+    }
+    
+    return 0;
+}
