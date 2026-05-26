dc71eb9254b11a438fe3ca20a793d4840869c73f
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-26 14:29:26 +0900

    [Passed] 동전이 있는 위치 (2ms, 0MB)

---
+++ code_tree/trail1/동전이 있는 위치/Where-coins-are-located.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    int arr[10][10] = {};
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++) {
+        int r, c;
+        cin >> r >> c;
+
+        arr[r - 1][c - 1] = 1;
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
