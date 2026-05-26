c2f39c39b8ff2b247ddc8ca87397fdc477874b52
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-26 14:59:34 +0900

    [Passed] 문자 개수 세기 (2ms, 0MB)

---
+++ code_tree/trail1/문자 개수 세기/count-char.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str;
+    char c;
+    int count = 0;
+
+    getline(cin, str);
+    cin >> c;
+
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] == c)
+            count++;
+    }
+    cout << count << endl;
+    return 0;
+}
