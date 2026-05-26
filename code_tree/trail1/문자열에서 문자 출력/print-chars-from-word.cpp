efe8a7728149c1b943dba9a1857bc1ca81daa13b
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-26 15:50:11 +0900

    [Passed] 문자열에서 문자 출력 (2ms, 0MB)

---
+++ code_tree/trail1/문자열에서 문자 출력/print-chars-from-word.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        cout << str[i] << endl;
+    } 
+    return 0;
+}
