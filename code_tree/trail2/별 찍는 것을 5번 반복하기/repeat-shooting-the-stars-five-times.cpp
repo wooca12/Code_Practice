6311ee0a0a805e62e8875e576833f41c2ca8fecf
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-27 22:37:53 +0900

    [Passed] 별 찍는 것을 5번 반복하기 (2ms, 0MB)

---
+++ code_tree/trail2/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void Print10Stars() {
+    for (int i = 0; i < 10; i++) {
+        cout << "*";
+    }
+    cout << endl;
+}
+int main() {
+    for (int i = 0; i < 5; i++) {
+        Print10Stars();
+    }
+    return 0;
+}
