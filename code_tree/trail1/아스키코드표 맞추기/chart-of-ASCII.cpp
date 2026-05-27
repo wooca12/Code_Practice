7730a6ea31762d999f515fd328a54d169e6f1974
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-27 16:43:04 +0900

    [Passed] 아스키코드표 맞추기 (2ms, 0MB)

---
+++ code_tree/trail1/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+
+    for (int i = 0; i < n; i++) {
+        int n;
+        cin >> n;
+
+        cout << char(n) << " ";
+    }
+    return 0;
+}
