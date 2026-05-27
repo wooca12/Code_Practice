63b689bec806267d565b12dc047a619bdd21a542
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-27 13:58:38 +0900

    [Passed] 특정 문자의 유무 (2ms, 0MB)

--- code_tree/trail1/특정 문자의 유무/specific-character-presence.cpp
+++ code_tree/trail1/특정 문자의 유무/specific-character-presence.cpp
@@ -6,14 +6,31 @@ int main() {
     string s;
     cin >> s;
 
-    if (s.find("ee") != string::npos)
+    // if (s.find("ee") != string::npos)
+    //     cout << "Yes ";
+    // else
+    //     cout << "No ";
+
+    // if (s.find("ab") != string::npos)
+    //     cout << "Yes";
+    // else 
+    //     cout << "No";
+
+    bool exist1 = false, exist2 = false;
+    for (int i = 0; i < s.length() - 1; i++) {
+        if (s.substr(i, 2) == "ee")
+            exist1 = "Yes";
+        if (s.substr(i, 2) == "ab")
+            exist2 = "Yes";
+    }
+
+    if (exist1) 
         cout << "Yes ";
     else
         cout << "No ";
-
-    if (s.find("ab") != string::npos)
+    if (exist2)
         cout << "Yes";
-    else 
+    else
         cout << "No";
     return 0;
 }
