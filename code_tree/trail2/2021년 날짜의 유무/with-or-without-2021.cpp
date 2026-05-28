97eec77e9410a0dbf659e86a45729e9f677ae653
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-28 13:05:27 +0900

    [Passed] 2021년 날짜의 유무 (2ms, 0MB)

--- code_tree/trail2/2021년 날짜의 유무/with-or-without-2021.cpp
+++ code_tree/trail2/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
 using namespace std;
 
-bool IsExistDay(int m, int d) {
-    if (m < 1 or m > 12)
-        return false;
-    
-    int arr[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    return d <= arr[m];
+
+int LastDayNumber(int m) {
+    if (m == 2)
+        return 28;
+    if (m == 4 || m == 6 || m == 9 || m == 11)
+        return 30;
+
+    return 31;
 }
+
+bool JudgeDay(int m, int d) {
+    if (m <= 12 && d <= LastDayNumber(m))
+        return true;
+
+    return false;
+}
+
 int main() {
     int m, d;
     cin >> m >> d;
     
-    if (IsExistDay(m, d)) 
+    if (JudgeDay(m, d)) 
         cout << "Yes";
     
     else
