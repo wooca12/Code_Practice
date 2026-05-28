aa5842c8b21e707a85bb6989555c0d92c0f196df
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-28 12:07:50 +0900

    [Passed] 함수를 이용한 연속부분수열 여부 판단하기 (2ms, 0MB)

--- code_tree/trail2/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
+++ code_tree/trail2/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
@@ -2,18 +2,34 @@
 
 using namespace std;
 
-bool IsSatisfied(int n1, int a[], int n2, int b[]) {
-    bool satisfied;
+// bool IsSatisfied(int n1, int a[], int n2, int b[]) {
+//     bool satisfied;
 
-    for (int i = 0; i < n1; i++) {
-        satisfied = true;
-        for (int j = 0; j < n2; j++) {
-            if (a[i + j] != b[j])
-                satisfied = false;
-        }
-        if (satisfied)
+//     for (int i = 0; i < n1; i++) {
+//         satisfied = true;
+//         for (int j = 0; j < n2; j++) {
+//             if (a[i + j] != b[j])
+//                 satisfied = false;
+//         }
+//         if (satisfied)
+//             return true;
+//     } 
+//     return false;
+// }
+
+bool IsSame(int idx, int a[], int n2, int b[]) {
+    for (int i = 0; i < n2; i++) {
+        if (a[idx + i] != b[i])
+            return false;
+    }
+    return true;
+}
+
+int IsSubSequence(int n1, int a[], int n2, int b[]) {
+    for (int i = 0; i <= n1 - n2; i++) {
+        if (IsSame(i, a, n2, b))
             return true;
-    } 
+    }
     return false;
 }
 
@@ -27,7 +43,12 @@ int main() {
     for (int i = 0; i < n2; i++) 
         cin >> b[i];
 
-    if (IsSatisfied(n1, a, n2, b))
+    // if (IsSatisfied(n1, a, n2, b))
+    //     cout << "Yes" << endl;
+    // else 
+    //     cout << "No" << endl;
+
+    if (IsSubSequence(n1, a, n2, b))
         cout << "Yes" << endl;
     else 
         cout << "No" << endl;
