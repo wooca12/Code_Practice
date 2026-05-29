c2f5c89592e55e36c595b24821f122d4b978ae36
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-29 18:22:04 +0900

    [Passed] 재귀함수를 이용한 별 출력 2 (2ms, 0MB)

---
+++ code_tree/trail2/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void PrintStar(int n) {
+    if (n == 0)
+        return;
+    
+    for (int i = 0; i < n; i++) {
+        cout << "* ";
+    }
+    cout << endl;
+
+    PrintStar(n - 1);
+
+    for (int i = 0; i < n; i++) {
+        cout << "* ";
+    }
+    cout << endl;
+}
+int main() {
+    int n;
+    cin >> n;
+
+    PrintStar(n);
+
+    return 0;
+}
