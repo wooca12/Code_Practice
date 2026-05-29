fc235f5eaaf4dbafe6c6933062fbe0688db56b9c
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-29 18:25:21 +0900

    [Passed] 1부터 특정 수까지의 합 2 (2ms, 0MB)

---
+++ code_tree/trail2/1부터 특정 수까지의 합 2/sum-from-1-to-a-certain-number-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int Sum(int n) {
+    if (n == 1) 
+        return 1;
+
+    return n + Sum(n - 1);
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << Sum(n) << endl;
+    
+    return 0;
+}
