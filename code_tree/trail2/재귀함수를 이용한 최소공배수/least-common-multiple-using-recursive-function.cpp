1d50a6260026df0069d5f88b71d8d2983eb062f6
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-29 22:49:06 +0900

    [Passed] 재귀함수를 이용한 최소공배수 (2ms, 0MB)

---
+++ code_tree/trail2/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[10];
+
+bool IsPrime(int n) {
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int GetGcd(int a, int b) {
+    int ans;
+    for (int i = 1; i <= min(a, b); i++) {
+        if (a % i == 0 && b % i == 0)
+            ans = i;
+    }
+    return ans;
+
+}
+
+int GetLcm(int a, int b) {
+    int gcd = GetGcd(a, b);
+
+    return ((a * b) / gcd);
+}
+
+int GetTotalLcm(int n) {
+    if (n == 0)
+        return arr[0];
+    int a =  GetLcm(arr[n], GetTotalLcm(n - 1));
+    return a;
+}
+
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << GetTotalLcm(n - 1);
+
+    return 0;
+}
