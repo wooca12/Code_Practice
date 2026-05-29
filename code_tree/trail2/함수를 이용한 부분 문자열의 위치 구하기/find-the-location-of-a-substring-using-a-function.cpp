0b0a5549d17403657bb5d786e537c2b0f890e11d
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-05-29 17:22:09 +0900

    [Passed] 함수를 이용한 부분 문자열의 위치 구하기 (2ms, 0MB)

--- code_tree/trail2/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.cpp
+++ code_tree/trail2/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.cpp
@@ -2,27 +2,48 @@
 #include <string>
 using namespace std;
 
-int SubSequence(string a, string b) {
-    int idx = -1;
 
-    int n1 = a.length();
-    int n2 = b.length();
+// ***** < MY CODE> *****
+// int FindIndex(string a, string b) {
+//     int idx = -1;
 
-    for (int i = 0; i < n1; i++) {
-        if (a.substr(i, n2) == b) {
-            idx = i;
-            break;
-        }
+//     int n1 = a.length();
+//     int n2 = b.length();
+
+//     for (int i = 0; i < n1; i++) {
+//         if (a.substr(i, n2) == b) {
+//             idx = i;
+//             break;
+//         }
+//     }
+
+//     return idx;
+// }
+
+bool IsSub(int start_idx, string a, string b) {
+    int len = (int) b.size();
+    
+    for (int i = 0; i < len; i++) {
+        if (a[start_idx + i] != b[i])
+            return false;
     }
+    return true;
+}
 
-    return idx;
+int FindIndex(string a, string b) {
+    int len = (int) a.size();
+    for (int i = 0; i < len; i++) {
+        if (IsSub(i, a, b)) 
+            return i;
+    }
+    return -1;
 }
 
 int main() {
     string a, b;
     cin >> a >> b;
 
-    int ans = SubSequence(a, b);
+    int ans = FindIndex(a, b);
     cout << ans << endl;
     return 0;
 }
