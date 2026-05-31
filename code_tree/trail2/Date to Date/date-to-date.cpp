16305d6e35c551df329faa62b543ee721885ec93
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-06-01 01:07:33 +0900

    [Passed] Date to Date (2ms, 0MB)

--- code_tree/trail2/Date to Date/date-to-date.cpp
+++ code_tree/trail2/Date to Date/date-to-date.cpp
@@ -1,29 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int days(int m) {
+int LastDay(int m) {
     if (m == 2)
         return 28;
     if (m == 4 || m == 6 || m == 9 || m == 11)
         return 30;
     return 31;
 }
+
+int NumOfDays(int m, int d) {
+    int total_days = 0;
+    for (int i = 1; i < m; i++) {
+        total_days += LastDay(i);
+    }
+    total_days += d;
+
+    return total_days;
+}
+
 int main() {
     int m1, d1, m2, d2;
     cin >> m1 >> d1;
     cin >> m2 >> d2;
 
-    int days1 = d1;
-    for (int i = 1; i < m1; i++) {
-        days1 += days(i);
-    }
-
-    int days2 = d2;
-    for (int i = 1; i < m2; i++) {
-        days2 += days(i);
-    }
-
-    cout << days2 - days1 + 1 << endl;
+   
+    cout << NumOfDays(m2, d2) - NumOfDays(m1, d1) + 1 << endl;
 
     return 0;
 }
