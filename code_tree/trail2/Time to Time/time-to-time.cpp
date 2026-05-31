6c58038543f1d80ba73d032af50e18795702761c
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-06-01 00:53:20 +0900

    [Passed] Time to Time (2ms, 0MB)

---
+++ code_tree/trail2/Time to Time/time-to-time.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int h1, m1, h2, m2;
+    cin >> h1 >> m1;
+    cin >> h2 >> m2;
+
+    int answer = 0;
+    int min = 0, hour = 0;
+
+    while(true) {
+        if (h1 == h2 && m1 == m2) 
+            break;
+
+        answer++;
+        m1++;
+        if (m1 == 60) {
+            h1++;
+            m1 = 0;
+        }
+    }
+
+    cout << answer << endl;
+
+    return 0;
+}
