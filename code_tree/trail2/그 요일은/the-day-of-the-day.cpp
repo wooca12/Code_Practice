6831e0be183d0bbcd1abb9fd5abc2e0e6cbdb373
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-06-01 01:46:09 +0900

    [Passed] 그 요일은 (2ms, 0MB)

--- code_tree/trail2/그 요일은/the-day-of-the-day.cpp
+++ code_tree/trail2/그 요일은/the-day-of-the-day.cpp
@@ -12,23 +12,51 @@ int NumOfDays(int m, int d) {
 
     return total_days;
 }
+
+int NumOfDay(string s) {
+    if (s == "Mon") return 0;
+    else if (s == "Tue") return 1;
+    else if (s == "Wed") return 2;
+    else if (s == "Thu") return 3;
+    else if (s == "Fri") return 4;
+    else if (s == "Sat") return 5;
+    return 6;
+}
+
 int main() {
     int m1, d1, m2, d2;
     string a;
     cin >> m1 >> d1 >> m2 >> d2;
     cin >> a;
 
-    string week[] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
 
-    int diff = NumOfDays(m2, d2) - NumOfDays(m1, d1);
+    // ############ < MY SOLUTION > ################
+    // string week[] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+
+    // int diff = NumOfDays(m2, d2) - NumOfDays(m1, d1);
+
+    // int cnt = 0;
+
+    // while(diff >= 0) {
+    //     if (week[diff % 7] == a)
+    //         cnt++;
+    //     diff--;
+    // }
+    // cout << cnt << endl;
+
+    int start_day = NumOfDays(m1, d1);
+    int end_day = NumOfDays(m2, d2);
+    int cur_day = NumOfDay("Mon");
 
     int cnt = 0;
 
-    while(diff >= 0) {
-        if (week[diff % 7] == a)
+    for (int i = start_day; i <= end_day; i++) {
+        if(cur_day == NumOfDay(a))
             cnt++;
-        diff--;
+        cur_day++;
+        cur_day %= 7;
     }
+
     cout << cnt << endl;
 
     return 0;
