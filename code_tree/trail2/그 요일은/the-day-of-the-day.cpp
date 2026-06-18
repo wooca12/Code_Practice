e37ed15bd598b5022637d94f7cfdb2a0c3489f1f
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-06-18 12:23:29 +0900

    [Passed] 그 요일은 (2ms, 0MB)

--- code_tree/trail2/그 요일은/the-day-of-the-day.cpp
+++ code_tree/trail2/그 요일은/the-day-of-the-day.cpp
@@ -1,63 +1,45 @@
 #include <iostream>
-#include <string>
+
 using namespace std;
 
-int NumOfDays(int m, int d) {
+int m1, m2, d1, d2;
+string A;
+
+int DayNums(int m1, int d1, int m2, int d2) {
     int days[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     int total_days = 0;
-
-    for (int i = 1; i < m; i++) 
+    for (int i = m1; i < m2; i++) 
         total_days += days[i];
-    total_days += d;
-
-    return total_days;
+    return total_days - d1 + d2;
 }
 
-int NumOfDay(string s) {
-    if (s == "Mon") return 0;
-    else if (s == "Tue") return 1;
-    else if (s == "Wed") return 2;
-    else if (s == "Thu") return 3;
-    else if (s == "Fri") return 4;
-    else if (s == "Sat") return 5;
-    return 6;
+int Day(string a) {
+    if (a == "Mon") return 0;
+    else if (a == "Tue") return 1;
+    else if (a == "Wed") return 2;
+    else if (a == "Thu") return 3;
+    else if (a == "Fri") return 4;
+    else if (a == "Sat") return 5;
+    else if (a == "Sun") return 6;
 }
 
 int main() {
-    int m1, d1, m2, d2;
-    string a;
     cin >> m1 >> d1 >> m2 >> d2;
-    cin >> a;
+    cin >> A;
 
+    int diff = DayNums(m1, d1, m2, d2);
 
-    // ############ < MY SOLUTION > ################
-    // string week[] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
-
-    // int diff = NumOfDays(m2, d2) - NumOfDays(m1, d1);
-
-    // int cnt = 0;
-
-    // while(diff >= 0) {
-    //     if (week[diff % 7] == a)
-    //         cnt++;
-    //     diff--;
-    // }
-    // cout << cnt << endl;
-
-    int start_day = NumOfDays(m1, d1);
-    int end_day = NumOfDays(m2, d2);
-    int cur_day = NumOfDay("Mon");
-
+    int num = Day(A);
     int cnt = 0;
 
-    for (int i = start_day; i <= end_day; i++) {
-        if(cur_day == NumOfDay(a))
+
+    for (int i = 0; i <= diff; i++) {
+        if ((i % 7) == num)
             cnt++;
-        cur_day++;
-        cur_day %= 7;
     }
 
     cout << cnt << endl;
+
 
     return 0;
 }
