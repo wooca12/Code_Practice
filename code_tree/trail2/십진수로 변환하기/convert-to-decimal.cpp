7dc1fccf639dbe8bc5908f34cf31ce8764a80ba7
wooca12 <64477736+wooca12@users.noreply.github.com>
2026-06-18 12:44:27 +0900

    [Passed] 십진수로 변환하기 (2ms, 0MB)

---
+++ code_tree/trail2/십진수로 변환하기/convert-to-decimal.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#define MAX_N 9
+using namespace std;
+
+
+int main() {
+    char binary[MAX_N];
+    cin >> binary;
+
+    int num = 0;
+
+    for (int i = 0; binary[i] != '\0' ; i++) {
+        num = num * 2 + (int(binary[i]) - int('0'));
+    }
+
+    cout << num << endl;
+
+
+    return 0;
+}
